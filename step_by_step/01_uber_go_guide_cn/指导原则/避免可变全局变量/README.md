4621a55048ee5bf36250b4ffd94da3a63e02179a
qoiuy <1010255393@qq.com>
2021-09-29 17:47:05 +0800

    避免全局变量被修改 。 如何避免呢？ 使用 依赖反转. 但是我并没有太理解什么叫依赖反转

---
+++ step_by_step/01_uber_go_guide_cn/指导原则/避免可变全局变量/README.md
@@ -0,0 +1,14 @@
+# golang 依赖注入
+
+使用选择依赖注入方式避免改变全局变量。 既适用于函数指针又适用于其他值类型
+
+原文是这样写的
+
+Avoid mutating global variables, instead opting for dependency injection. This applies to function pointers as well as other kinds of values.
+
+
+Avoid(避免) mutating(变化) global variables,
+instead(代替) opting(选择) for dependency(依赖) injection(注入).
+This applies(应用) to function(工作) pointers(指针)
+as well(同样地) as other kinds of values.
+
---
+++ step_by_step/01_uber_go_guide_cn/指导原则/避免可变全局变量/t3_test.go
@@ -0,0 +1,52 @@
+package 避免可变全局变量
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+// 全局变量
+var _timeNow = time.Now
+
+// ============================== 分割线 ========================
+func sign(msg string) string {
+	now := _timeNow()
+	return fmt.Sprintf("%s:%v", msg, now)
+}
+
+func Test_3(t *testing.T) {
+	oldTimeNow := _timeNow
+	_timeNow = func() time.Time {
+		return someFixedTime
+	}
+	defer func() { _timeNow = oldTimeNow }()
+
+}
+
+// ============================== 分割线 ========================
+// 使用一个结果体包含全局变量 ?  单例?
+type singer struct {
+	now func() time.Time
+}
+
+func newSinger() *singer {
+	return &singer{now: time.Now}
+}
+
+// ============================== 分割线 ========================
+
+func (s *singer) Sign(msg string) string {
+	now := s.now()
+	return fmt.Sprintf("%s:%v", msg, now)
+}
+
+func Test_4(t *testing.T) {
+	s := newSinger()
+	s.now = func() time.Time {
+		return someFixedTime
+	}
+}
+
+// ============================== 分割线 ========================
+var someFixedTime time.Time
