adfb4a9c9478abffc0a6dbc19570b13f63d6a472
qoiuy <1010255393@qq.com>
2021-10-20 10:14:02 +0800

    增加理解

--- step_by_step/01_uber_go_guide_cn/性能/避免字符串到字节的转换/uber_demo_learn/demo1/main/main_test.go
+++ step_by_step/01_uber_go_guide_cn/性能/避免字符串到字节的转换/uber_demo_learn/demo1/main/main_test.go
@@ -7,7 +7,10 @@ import (
 
 func BenchmarkBad(b *testing.B) {
 	var w = bytes.Buffer{}
-	b.ResetTimer() // 重置定时器
+	////b.ResetTimer() // 重置定时器
+	//for i := 0; i < b.N; i++ {
+	//	w.Write([]byte("Hello world"))
+	//}
 	for i := 0; i < b.N; i++ {
 		w.Write([]byte("Hello world"))
 	}
@@ -15,9 +18,16 @@ func BenchmarkBad(b *testing.B) {
 }
 func BenchmarkGood(b *testing.B) {
 	var w = bytes.Buffer{}
+	//data := []byte("Hello world")
+	////b.ResetTimer() // 重置定时器
+	//for i := 0; i < b.N; i++ {
+	//	w.Write(data)
+	//}
 	data := []byte("Hello world")
-	b.ResetTimer() // 重置定时器
 	for i := 0; i < b.N; i++ {
 		w.Write(data)
 	}
 }
+
+// go test -bench .  -benchtime=50000000x
+// 思远：很多只会执行一次的代码 就不要这样写 这样写会导致创建多次
